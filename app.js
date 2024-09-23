c91cefbe8690ab1f1f82deed6d08c2403cca8ac8
Mrityunjay deepak <mrityunjaydeepak22@gmail.com>
2024-09-23 14:03:35 +0530

    updated dateandtimefields
    Co-Authored-By: EESHEEPAL  SINGH <83159540+eesheesingh@users.noreply.github.com>

--- app.js
+++ app.js
@@ -31,17 +31,7 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 
-const formatDate = (date) => {
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JavaScript
-  const year = date.getFullYear();
-  
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
 
-  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
-};
 
 // MongoDB User Schema and Model
 const userSchema = new mongoose.Schema({
@@ -62,10 +52,7 @@ const groupSchema = new mongoose.Schema({
       mobileNumber: { type: String, required: true },
     }
   ],
-  dateCreatedOn: {
-    type: String, // Store the date and time as a string
-    default: () => formatDate(new Date()), // Format the date and time as DD/MM/YYYY HH:mm:ss
-  },
+  dateCreatedOn: { type: Date, default: Date.now },
 });
 
 const Group = mongoose.model('Group', groupSchema);
