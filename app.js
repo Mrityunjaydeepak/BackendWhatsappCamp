16d62e08fa3e4243501c4027221b171900e045e2
Mrityunjay deepak <mrityunjaydeepak22@gmail.com>
2024-09-24 01:36:05 +0530

    Revert "time fields in groupSchema"

--- app.js
+++ app.js
@@ -44,12 +44,7 @@ const formatDate = (date) => {
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JavaScript
   const year = date.getFullYear();
-  
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-
-  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  return `${day}/${month}/${year}`;
 };
 
 // MongoDB Group Schema and Model
@@ -58,17 +53,16 @@ const groupSchema = new mongoose.Schema({
   users:[
     {
       userId: { type: String, required: true },
-      raName: { type: String, default: null },
+      raName:{type:String,default:null},
       name: { type: String, default: null },
       mobileNumber: { type: String, required: true },
     }
   ],
   dateCreatedOn: {
-    type: String, // Store the date and time as a string
-    default: () => formatDate(new Date()), // Format the date and time as DD/MM/YYYY HH:mm:ss
+    type: String, // Store the date as a string
+    default: () => formatDate(new Date()), // Format the date as DD/MM/YYYY before saving
   },
 });
-
 const Group = mongoose.model('Group', groupSchema);
 
 // MongoDB Schedule Schema and Model
